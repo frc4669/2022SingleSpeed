e3c82447bc84c8f6d6b96114cd3240e3aaea022f
Alvin-He <shinypeach36@gmail.com>
2022-12-09 21:43:31 -0800

    Revert "trying to make sim work"
    This reverts commit 6d51e752aba69ddc057f45109e2383abe2d0817e.

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -7,8 +7,8 @@
 #include <frc2/command/RamseteCommand.h>
 #include <frc2/command/InstantCommand.h>
 #include <frc2/command/SequentialCommandGroup.h>
-// #include <pathplanner/lib/PathPlannerTrajectory.h>
-// #include <pathplanner/lib/PathPlanner.h>
+#include <pathplanner/lib/PathPlannerTrajectory.h>
+#include <pathplanner/lib/PathPlanner.h>
 #include <frc/smartdashboard/SmartDashboard.h>
 
 #include "commands/GoToTarget.h"
@@ -52,32 +52,30 @@ void RobotContainer::ConfigureButtonBindings() {
 }
 
 frc2::Command* RobotContainer::GetAutonomousCommand() {
-  return nullptr; 
+  pathplanner::PathPlannerTrajectory autoTrajectory = pathplanner::PathPlanner::loadPath(TRAJECTORY_NAME, DriveConstants::kMaxAutoSpeed, DriveConstants::kMaxAutoAccel);
+  frc::Trajectory WPItrajectory = autoTrajectory.asWPILibTrajectory();
 
-//   pathplanner::PathPlannerTrajectory autoTrajectory = pathplanner::PathPlanner::loadPath(TRAJECTORY_NAME, DriveConstants::kMaxAutoSpeed, DriveConstants::kMaxAutoAccel);
-//   frc::Trajectory WPItrajectory = autoTrajectory.asWPILibTrajectory();
+  frc::SmartDashboard::PutNumber("Initial Auto Rotation", WPItrajectory.InitialPose().Rotation().Degrees().value());
 
-//   frc::SmartDashboard::PutNumber("Initial Auto Rotation", WPItrajectory.InitialPose().Rotation().Degrees().value());
+  m_drivetrain.ResetOdometry(WPItrajectory.InitialPose(), WPItrajectory.InitialPose().Rotation());
 
-//   m_drivetrain.ResetOdometry(WPItrajectory.InitialPose(), WPItrajectory.InitialPose().Rotation());
+  m_drivetrain.GetField()->GetObject("trajectory")->SetTrajectory(WPItrajectory);
 
-//   m_drivetrain.GetField()->GetObject("trajectory")->SetTrajectory(WPItrajectory);
+  frc2::RamseteCommand followTrajectory {
+    autoTrajectory.asWPILibTrajectory(),
+    [this] { return m_drivetrain.GetOdometryPose(); },
+    frc::RamseteController(),
+    frc::SimpleMotorFeedforward<units::meters>(DriveConstants::ks, DriveConstants::kv, DriveConstants::ka),
+    frc::DifferentialDriveKinematics(DriveConstants::kTrackWidth),
+    [this] { return m_drivetrain.GetWheelSpeeds(); },
+    frc2::PIDController(DriveConstants::kp, DriveConstants::ki, DriveConstants::kd),
+    frc2::PIDController(DriveConstants::kp, DriveConstants::ki, DriveConstants::kd),
+    [this] (auto left, auto right) { frc::SmartDashboard::PutNumber("Auto Voltage L", left.value()); frc::SmartDashboard::PutNumber("Auto Voltage R", right.value()); return m_drivetrain.TankDriveVolts(left, right); },
+    { &m_drivetrain }
+  };
 
-//   frc2::RamseteCommand followTrajectory {
-//     autoTrajectory.asWPILibTrajectory(),
-//     [this] { return m_drivetrain.GetOdometryPose(); },
-//     frc::RamseteController(),
-//     frc::SimpleMotorFeedforward<units::meters>(DriveConstants::ks, DriveConstants::kv, DriveConstants::ka),
-//     frc::DifferentialDriveKinematics(DriveConstants::kTrackWidth),
-//     [this] { return m_drivetrain.GetWheelSpeeds(); },
-//     frc2::PIDController(DriveConstants::kp, DriveConstants::ki, DriveConstants::kd),
-//     frc2::PIDController(DriveConstants::kp, DriveConstants::ki, DriveConstants::kd),
-//     [this] (auto left, auto right) { frc::SmartDashboard::PutNumber("Auto Voltage L", left.value()); frc::SmartDashboard::PutNumber("Auto Voltage R", right.value()); return m_drivetrain.TankDriveVolts(left, right); },
-//     { &m_drivetrain }
-//   };
-
-//   return new frc2::SequentialCommandGroup(
-//     std::move(followTrajectory)/*,
-//     frc2::InstantCommand([this] { m_drivetrain.TankDriveVolts(0_V, 0_V); })*/
-//   );
+  return new frc2::SequentialCommandGroup(
+    std::move(followTrajectory)/*,
+    frc2::InstantCommand([this] { m_drivetrain.TankDriveVolts(0_V, 0_V); })*/
+  );
 }
